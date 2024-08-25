61f4eb3c885ebee17d522f3f6c793883824f789f
Jezmem <tomzarb98@gmail.com>
2024-08-25 19:56:08 +0200

    Nouvelle question

--- migrations/Version20240823155413.php
+++ migrations/Version20240823155413.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240822202951 extends AbstractMigration
+final class Version20240823155413 extends AbstractMigration
 {
     public function getDescription(): string
     {
